404d7d291458adcc9c3d022578e258cf445f18a8
Anvar <anvaranuckd@gmail.com>
2021-10-21 18:33:28 +0000

    reverse of an array

--- main.js
+++ main.js
@@ -1,13 +1,14 @@
-var a = 5;
-if (a) {
-  var a = 6;
-  console.log(a);
+//reverse array
+const reverseArray = (arr) => {
+  const length = arr.length;
+  for (let i = 0; i < length / 2; i++) {
+    let other = length - i - 1;
+    let temp = arr[i];
+    arr[i] = arr[other];
+    arr[other] = temp;
+  }
+  return arr;
 }
-console.log(a);
 
-var a = 5;
-if (a) {
-  var a = 6;
-  console.log(a);
-}
-console.log(a);
+const ar = [52, 35, 89, 10, 55, 0, 7, 3, 9, 4, 0, 5, 8, 9, 3, 'abc', 'te', 98];
+console.log(reverseArray(ar));
